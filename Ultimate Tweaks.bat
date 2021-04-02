3b10d91fe24d39ba113f0dc1bff219e2247212ea
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2021-04-02 21:34:49 +0600

    [MISC] Moved "programs" folder to bin folder

--- Ultimate Tweaks.bat
+++ Ultimate Tweaks.bat
@@ -3,7 +3,7 @@ SET "VERSION=v2.0"
 TITLE Ultimate System Tweaks %VERSION% by Ahsan
 SET "Current_Dir=%~dp0"
 SET "Bin_Dir=%Current_Dir%\bin"
-SET "Programs_Dir=%Current_Dir%\programs"
+SET "Programs_Dir=%Bin_Dir%\programs"
 PUSHD %~DP0
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
 Md "%WinDir%\System32\test_permissions" 2>NUL||(WScript "%Bin_Dir%\Elevate.vbs" && EXIT)
