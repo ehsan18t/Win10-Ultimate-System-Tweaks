10e3c87f60d070b4ffa1ecbfd4c9cc6ab7751991
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2021-04-03 17:00:42 +0600

    [ADDED] Unsupported Windows version & architecture detection

--- Ultimate Tweaks.bat
+++ Ultimate Tweaks.bat
@@ -1,15 +1,17 @@
 @ECHO OFF
 SET "VERSION=v2.0"
 TITLE Ultimate System Tweaks %VERSION% by Ahsan
+
 SET "Current_Dir=%~dp0"
 SET "Bin_Dir=%Current_Dir%\bin"
 SET "Programs_Dir=%Bin_Dir%\programs"
+
 PUSHD %~DP0
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
 Md "%WinDir%\System32\test_permissions" 2>NUL||(WScript "%Bin_Dir%\Elevate.vbs" && EXIT)
 Rd "%WinDir%\System32\test_permissions" 2>NUL
-COLOR 0A
 
+COLOR 0A
 ECHO.
 ECHO  		**********************************************
 ECHO  		**********************************************
@@ -20,6 +22,24 @@ ECHO  		**********************************************
 ECHO  		**********************************************
 ECHO.
 ECHO.
+
+IF NOT EXIST "%SystemDrive%\Program Files (x86)" (
+	SET "WINVER=(32-BIT)"
+	GOTO :TWEAKS_NOT_SUPPORTED
+)
+
+FOR /F "TOKENS=2 DELIMS=[]" %%X IN ('ver') DO (SET "WINVER=%%X" && GOTO :WIN_VER_SEARCH_BREAK)
+:WIN_VER_SEARCH_BREAK
+SET "WINVER=%WINVER:Version =%"
+IF %WINVER% LSS 10 (
+	:TWEAKS_NOT_SUPPORTED
+	ECHO.
+	ECHO  THIS TWEAKS IS NOT SUPPORTED IN THIS WINDOWS %WINVER%!
+	ECHO.
+	ECHO  Press any key to exit...
+	PAUSE >NUL 2>&1
+	EXIT
+)
 
 %Bin_Dir%\Codes.bat 2>&1 | %Bin_Dir%\wtee.exe %Current_Dir%\logs.txt
 
