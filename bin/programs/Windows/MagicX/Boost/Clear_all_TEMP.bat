729f039dd4e43e8c0fac2f159d69b1961035c1bb
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2021-04-05 22:33:00 +0600

    [IMPROVED] Taking the "Boost" to a new level

--- bin/programs/Windows/MagicX/Boost/Clear_all_TEMP.bat
+++ bin/programs/Windows/MagicX/Boost/Clear_all_TEMP.bat
@@ -1,12 +1,18 @@
 @ECHO OFF
-TITLE Clear all TEMP Files and Folders
-DEL /S /Q "%TMP%\*.*"
-DEL /S /Q "%TEMP%\*.*"
-DEL /S /Q "%WINDIR%\Temp\*.*"
-DEL /S /Q "%USERPROFILE%\Local Settings\Temp\*.*"
-DEL /S /Q "%LOCALAPPDATA%\Temp\*.*"
-IF EXIST "%SystemRoot%\MagicX\NecessaryLib\EcMenu.ini" DEL "%SystemRoot%\MagicX\NecessaryLib\EcMenu.ini"
-IF EXIST "%SystemRoot%\MagicX\NecessaryLib\Items.ini" DEL "%SystemRoot%\MagicX\NecessaryLib\Items.ini"
+TITLE TEMP Files^/Folders ^& RAM Cleaner
 "%SystemRoot%\MagicX\NecessaryLib\EcMenu.exe" /TempClean
-nircmd emptybin
+TASKKILL /F /FI "STATUS EQ NOT RESPONDING"
+EmptyStandbyList workingsets
+EmptyStandbyList priority0standbylist
+EmptyStandbyList workingsets
+EmptyStandbyList standbylist
+EmptyStandbyList workingsets
+EmptyStandbyList modifiedpagelist
+EmptyStandbyList workingsets
+DEL /S /Q /F "%TMP%\*"
+DEL /S /Q /F "%TEMP%\*"
+DEL /S /Q /F "%WINDIR%\Temp\*"
+DEL /S /Q /F "%USERPROFILE%\Local Settings\Temp\*"
+DEL /S /Q /F "%LOCALAPPDATA%\Temp\*"
+NIRCMD EMPTYBIN
 "%SystemRoot%\MagicX\NecessaryLib\EcMenu.exe" /Admin /ReduceMemory
